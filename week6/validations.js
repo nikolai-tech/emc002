48644e967a05ce9cf72a92198cdbe2f76f4b2b40
nikolai-tech <nikolai.tristan.c@gmail.com>
2024-02-02 20:36:53 +0800

    new changes

--- week6/validations.js
+++ week6/validations.js
@@ -1,69 +1,130 @@
-// User data list
-var users = [];
-// Function to add a user
-function addUser() 
-{
- var username = document.getElementById("username").value;
- var password = document.getElementById("password").value;
- // Validate username and password
- if (username === "" || password === "") 
- {
- alert("Please enter both username and password.");
- return;
- }
+// Select the username and password input elements
+var username = $("#username");
+var password = $("#password");
 
- // Username validation
-if (username.length < 6 || username.match(/[^A-Za-z0-9\s_]/)) {
-alert("Username must be at least 6 characters long. Username must not contain special characters except underscore.");
-return;
+// Select the image elements for each rule
+var rule1 = $(".rule1 img");
+var rule2 = $(".rule2 img");
+var rule3 = $(".rule3 img");
+var rule4 = $(".rule4 img");
+var rule5 = $(".rule5 img");
+var rule6 = $(".rule6 img");
+var rule7 = $(".rule7 img");
+
+// Select the button element
+var button = $("button");
+
+// Define the regular expressions for the rules
+var usernameRegex = /^[a-zA-Z0-9_]{6,}$/; // At least 6 alphanumeric or underscore characters
+var passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/; // At least 8 characters, including uppercase, lowercase, number, and special character
+
+// Define a function to validate the input values
+function validate() {
+  // Get the input values
+  var usernameVal = username.val();
+  var passwordVal = password.val();
+
+  // Check if the username matches the rule 1
+  if (usernameVal.length >= 6) {
+    // If yes, change the image to rightlogo
+    rule1.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule1.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the username matches the rule 2
+  if (usernameVal.indexOf("_") != -1 || usernameVal.match(/[a-zA-Z0-9]/g).length == usernameVal.length) {
+    // If yes, change the image to rightlogo
+    rule2.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule2.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the password matches the rule 3
+  if (passwordVal.length >= 8) {
+    // If yes, change the image to rightlogo
+    rule3.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule3.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the password matches the rule 4
+  if (passwordVal.match(/[A-Z]/)) {
+    // If yes, change the image to rightlogo
+    rule4.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule4.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the password matches the rule 5
+  if (passwordVal.match(/[a-z]/)) {
+    // If yes, change the image to rightlogo
+    rule5.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule5.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the password matches the rule 6
+  if (passwordVal.match(/[0-9]/)) {
+    // If yes, change the image to rightlogo
+    rule6.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule6.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if the password matches the rule 7
+  if (passwordVal.match(/[!@#$%^&*]/)) {
+    // If yes, change the image to rightlogo
+    rule7.attr("src", "images/rightlogo.jpg");
+  } else {
+    // If no, change the image to wronglogo
+    rule7.attr("src", "images/wronglogo.jpeg");
+  }
+
+  // Check if all the rules are satisfied
+  if (usernameVal.length >= 6 && usernameVal.indexOf("_") != -1 || usernameVal.match(/[a-zA-Z0-9]/g).length == usernameVal.length && passwordRegex.test(passwordVal)) {
+    // If yes, enable the button
+    button.prop("disabled", false);
+  } else {
+    // If no, disable the button
+    button.prop("disabled", true);
+  }
 }
 
- // Password validation
- if (password.length < 8 ||
- !password.match(/[A-Z]/) ||
- !password.match(/[a-z]/) ||
- !password.match(/[0-9]/) ||
- !password.match(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/)) {
- alert("Password must contain at least 8 characters, including an uppercase letter, a lowercase letter, a number, and a special character.");
- return;
- }
- // Check if the username already exists
- for (var i = 0; i < users.length; i++) 
- {
- if (users[i].username === username) 
- {
- alert("Username already exists.");
- alert("Please input another username.");
- return;
- }
- }
- // Add the user to the array
- users.push({ username: username, password: password });
- alert("User added successfully.");
-}
-// Function to validate a user
-function validateUser() {
- var username = document.getElementById("username").value;
- var password = document.getElementById("password").value;
- // Check if the username and password exist in the array
- for (var i = 0; i < users.length; i++) 
- {
- if (users[i].username === username && users[i].password === password) 
- {
- alert("This user already exists.");
- alert("Login successful.");
- return;
- }
- else if(users[i].username !== username || users[i].password !== password) 
- {
- alert("Login unsuccessful. Username or password is incorrect. Please try again.");
- return;
- }
- }
- alert("This user doesn't exist in the list yet.");
-}
-// Function to clear the input fields
-function clearFields() {
- document.getElementById("username").value = "";
- document.getElementById("password").value = "";
-}
+// Define a function to reset the rules
+function reset() {
+    // Change all the images to wronglogo
+    rule1.attr("src", "images/wronglogo.jpeg");
+    rule2.attr("src", "images/wronglogo.jpeg");
+    rule3.attr("src", "images/wronglogo.jpeg");
+    rule4.attr("src", "images/wronglogo.jpeg");
+    rule5.attr("src", "images/wronglogo.jpeg");
+    rule6.attr("src", "images/wronglogo.jpeg");
+    rule7.attr("src", "images/wronglogo.jpeg");
+  }
+  
+  // Call the reset function after displaying the alert message
+  function register() {
+    // Clear the input values
+    username.val("");
+    password.val("");
+  
+    // Display an alert message
+    alert("Registration successful");
+  
+    // Reset the rules
+    reset();
+  }
+  
+// Call the validate function when the input values change
+username.on("input", validate);
+password.on("input", validate);
+
+// Call the register function when the button is clicked
+button.on("click", register);
